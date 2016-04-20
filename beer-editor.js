913f861c38494d05e106b3cb10ac08d3de95164a
Frédéric Sureau <frederic.sureau@gmail.com>
2016-04-20 18:29:06 +0200

    Add spices and refactor style

--- beer-editor.js
+++ beer-editor.js
@@ -1,10 +1,13 @@
 angular.module('beerEditor', [])
   .controller('BeerEditorController', function($scope, $http) {
 
+    $scope.copy = angular.copy;
+    $scope.bh = Brauhaus;
+
     // Storage and default values
-    $scope.autostore = function(key, default_value) {
-      this[key] = localStorage[key] ? JSON.parse(localStorage[key]) : 
-                                      angular.copy(default_value);
+    $scope.autostore = function(key, defaultValue) {
+      this[key] = localStorage[key] ? JSON.parse(localStorage[key])
+                                    : angular.copy(defaultValue);
 
       this.$watch(key, function(value) {
         localStorage[key] = JSON.stringify(value);
@@ -12,12 +15,12 @@ angular.module('beerEditor', [])
     };
 
     var DEFAULT_EQUIPMENT = {
-      boil_rate: 10
+      boilRate: 10
     };
-  
+
     var DEFAULT_RECIPE = {
-      batchSize: 50,
-      boil_time: 60,
+      batchSize: 20,
+      boilTime: 60,
       fermentables: [],
       spices: []
     };
@@ -31,30 +34,52 @@ angular.module('beerEditor', [])
     $scope.autostore('equipment', DEFAULT_EQUIPMENT);
 
     // Calculations
-    var boil_size = function(recipe, equipment) {
+    var boilSize = function(recipe, equipment) {
       return recipe.batchSize +
-             recipe.batchSize*equipment.boil_rate/100
-                             *recipe.boil_time/60;
+             recipe.batchSize * equipment.boilRate/100
+                              * recipe.boilTime/60;
     };
 
-    var update_calculations = function() {
-      var recipe = $scope.recipe;
-      var equipment = $scope.equipment;
-
-      var calculations = new Brauhaus.Recipe(recipe);
-      calculations.boilSize = boil_size(recipe, equipment);
-      calculations.calculate();
-
-      $scope.calculations = calculations;
+    var updateCalculations = function() {
+      $scope.calculations = new Brauhaus.Recipe($scope.recipe);
+      $scope.calculations.boilSize = boilSize($scope.recipe, $scope.equipment);
+      $scope.calculations.calculate();
+      
+      $scope.calculations.buToGu = $scope.calculations.buToGu || 0;
     };
 
-    $scope.$watch('recipe', update_calculations, true);
-    $scope.$watch('equipment', update_calculations, true);
+    $scope.$watch('recipe', updateCalculations, true);
+    $scope.$watch('equipment', updateCalculations, true);
+
+    $scope.spiceBitterness = function(spice, calculations) {
+      return (new Brauhaus.Spice(spice)
+        .bitterness('tinseth', calculations.og, calculations.batchSize));
+    };
 
     // Ingredients
+    $scope.fermentablesTypes = [
+      {filter: {type:'dme'}, name: "Extraits de malt"},
+      {filter: {type:'base'}, name: "Malts de base"},
+      {filter: {type:'cara'}, name: "Malts caramel"},
+      {filter: {type:'roasted'}, name: "Malts grillés"},
+      {filter: {type:'flakes'}, name: "Flocons"},
+      {filter: {type:'sugar'}, name: "Sucres"}
+    ];
     $scope.fermentables = [];
     $http.get('fermentables.json').then(function(response) {
       $scope.fermentables = response.data;
+    });
+
+    $scope.spicesTypes = [
+      {filter: {type:'hop', origin:'us'}, name: "Houblons Américains"},
+      {filter: {type:'hop', origin:'uk'}, name: "Houblons Anglais"},
+      {filter: {type:'hop', origin:'de'}, name: "Houblons Allemands"},
+      {filter: {type:'hop', origin:'other'}, name: "Houblons d'ailleurs"},
+      {filter: {type:'!hop', origin:'other'}, name: "Autres"}
+    ];
+    $scope.spices = [];
+    $http.get('spices.json').then(function(response) {
+      $scope.spices = response.data;
     });
 
   });
