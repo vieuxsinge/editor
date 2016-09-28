62cdee7499cb00b8950fdfe006064b33e8c9eebd
Frédéric Sureau <frederic.sureau@gmail.com>
2016-09-28 15:46:44 +0200

    Persist ingredients

--- editor/conf/persistence/persistence.js
+++ editor/conf/persistence/persistence.js
@@ -1,5 +1,5 @@
 angular.module('editor.conf.persistence', ['editor.services.persistence',
-  'editor.data.recipes'])
+  'editor.data.recipes', 'editor.data.ingredients'])
   .config(function(persistenceProvider) {
   
     persistenceProvider.url = 'https://kinto.notmyidea.org/v1'
@@ -8,8 +8,12 @@ angular.module('editor.conf.persistence', ['editor.services.persistence',
     persistenceProvider.bucket = 'default';
   
   })
-  .run(function(persistence, recipes) {
+  .run(function(persistence, recipes, ingredients) {
   
     persistence.persist(recipes, 'recipes');
+    persistence.persist(ingredients.fermentables, 'fermentables');
+    persistence.persist(ingredients.hops, 'hops');
+    persistence.persist(ingredients.yeast, 'yeast');
+    persistence.persist(ingredients.others, 'others');
   
   });
--- editor/data/ingredients/ingredients.js
+++ editor/data/ingredients/ingredients.js
@@ -12,31 +12,5 @@ angular.module('editor.data.ingredients', ['editor.services.collection'])
         others: "Autres"
       }
     };
-  })
-  .run(function($http, ingredients) {
-
-    var addIngredient = function(category, collection) {
-      return function(ingredient) {
-        ingredient.category = category;
-        collection.create(ingredient);
-      };
-    };
-
-    $http.get('resources/yeast.json').then(function(response) {
-      angular.forEach(response.data, addIngredient('yeast', ingredients.yeast));
-    });
-
-    $http.get('resources/others.json').then(function(response) {
-      angular.forEach(response.data, addIngredient('others', ingredients.others));
-    });
-
-    $http.get('resources/hops.json').then(function(response) {
-      angular.forEach(response.data, addIngredient('hops', ingredients.hops));
-    });
-
-    $http.get('resources/fermentables.json').then(function(response) {
-      angular.forEach(response.data, addIngredient('fermentables', ingredients.fermentables));
-    });
-
   });
 
