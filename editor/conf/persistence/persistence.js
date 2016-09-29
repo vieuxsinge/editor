82628161ee9b802296223aeb152ba4e5cc69fc9a
Frédéric Sureau <frederic.sureau@gmail.com>
2016-09-29 18:29:30 +0200

    conf.persistence: Persist changes on settings

--- editor/conf/persistence/persistence.js
+++ editor/conf/persistence/persistence.js
@@ -1,5 +1,5 @@
 angular.module('editor.conf.persistence', ['editor.services.persistence',
-  'editor.data.recipes', 'editor.data.ingredients'])
+  'editor.data.recipes', 'editor.data.ingredients', 'editor.data.settings'])
   .config(function(persistenceProvider) {
   
     persistenceProvider.url = 'https://kinto.notmyidea.org/v1'
@@ -8,12 +8,13 @@ angular.module('editor.conf.persistence', ['editor.services.persistence',
     persistenceProvider.bucket = 'default';
   
   })
-  .run(function(persistence, recipes, ingredients) {
+  .run(function(persistence, recipes, ingredients, settings) {
   
     persistence.persistCollection(recipes, 'recipes');
     persistence.persistCollection(ingredients.fermentables, 'fermentables');
     persistence.persistCollection(ingredients.hops, 'hops');
     persistence.persistCollection(ingredients.yeast, 'yeast');
     persistence.persistCollection(ingredients.others, 'others');
+    persistence.persistObject(settings, 'settings');
   
   });
