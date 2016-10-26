0791f5a8229d931a6ca9c7a2f938009a734d7e78
Frédéric Sureau <frederic.sureau@gmail.com>
2016-10-26 17:42:05 +0200

    conf.persistence: Persist scaling option as a global preference

--- editor/conf/persistence/persistence.js
+++ editor/conf/persistence/persistence.js
@@ -1,5 +1,6 @@
 angular.module('editor.conf.persistence', ['editor.services.persistence',
-  'editor.data.equipments', 'editor.data.recipes', 'editor.data.ingredients'])
+  'editor.data.equipments', 'editor.data.recipes', 'editor.data.ingredients',
+  'editor.data.settings'])
   .config(function(persistenceProvider) {
   
     persistenceProvider.url = 'https://kinto.notmyidea.org/v1'
@@ -8,7 +9,7 @@ angular.module('editor.conf.persistence', ['editor.services.persistence',
     persistenceProvider.bucket = 'default';
   
   })
-  .run(function(persistence, recipes, equipments, ingredients) {
+  .run(function(persistence, recipes, equipments, ingredients, settings) {
   
     persistence.persistCollection(recipes, 'recipes');
     persistence.persistCollection(ingredients.fermentables, 'fermentables');
@@ -16,5 +17,6 @@ angular.module('editor.conf.persistence', ['editor.services.persistence',
     persistence.persistCollection(ingredients.yeast, 'yeast');
     persistence.persistCollection(ingredients.others, 'others');
     persistence.persistCollection(equipments, 'equipments');
+    persistence.persistObject(settings.global, 'settings.global');
   
   });
